783f40ffbce7d96099e82b9b0b9de6b5bde4710e
Diana Cortes <diana.cortes@COL-MC-220926D.local>
2022-11-10 12:49:44 -0500

    cart context and shopping icon

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,23 +1,37 @@
-import { Fragment } from 'react';
-import { Outlet, Link } from 'react-router-dom'
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import './navigation.styles.scss';
+import { Fragment, useContext } from "react";
+import { Outlet, Link } from "react-router-dom";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropDown from "../../components/cart-dropdown/cart-dropdown.component";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/user.context";
+import { CartContext, CartProvider } from "../../contexts/cart.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+import "./navigation.styles.scss";
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
+
   return (
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container'>
-        <CrwnLogo className='logo'/>
+      <div className="navigation">
+        <Link className="logo-container">
+          <CrwnLogo className="logo" />
         </Link>
-        <div className='nav-links-container'>
-            <Link className='nav-link' to='/shop'>
-                Shop
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            Shop
+          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={ signOutUser }>Sign Out</span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              Sign In
             </Link>
-            <Link className='nav-link' to='/auth'>
-                Sign In
-            </Link>
+          )}
+          <CartIcon />
         </div>
+        { isCartOpen && <CartDropDown /> }
       </div>
       <Outlet />
     </Fragment>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword,
-signInWithEmailAndPassword } from 'firebase/auth';
+signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -61,3 +61,7 @@ const firebaseConfig = {
 
     return await signInWithEmailAndPassword(auth, email, password)
   }
+
+  export const signOutUser = async () => await signOut(auth);
+
+  export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
